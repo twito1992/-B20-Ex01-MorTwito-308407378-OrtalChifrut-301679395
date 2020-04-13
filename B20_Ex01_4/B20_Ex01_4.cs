f6e5b8655e24c03c961b12f6e8f6967ad80a4bdf
Mor Twito <twito1992@gmail.com>
2020-04-13 18:20:21 +0300

    stratig using git

---
+++ B20_Ex01_4/B20_Ex01_4.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Text;
+
+namespace B20_Ex01_4
+{
+    class B20_Ex01_4
+    {
+        public static void Main()
+        {
+            stringAnalysis();
+            Console.WriteLine("Please press 'ENTER' to exit...");
+            Console.ReadLine();
+        }
+
+        private static bool isPalindrome(string i_str)
+        {
+            bool is_Palindrome = false;
+            if (i_str.Length <= 1)
+            {
+                return !is_Palindrome;
+            }
+            else
+            {
+                if (i_str[0] != i_str[i_str.Length - 1])
+                {
+                    return is_Palindrome;
+                }
+                else
+                {
+                    return isPalindrome(i_str.Substring(1,i_str.Length - 2));
+                }
+            }
+        }
+        private static bool isNumDivBy5(string i_userNumber)
+        {
+            int userNumber = 0;
+            bool isNumber = int.TryParse(i_userNumber,out userNumber);
+            bool divBy5 = false;
+            if (isNumber == true)
+            {
+                if(userNumber %5 == 0)
+                //if ((userNumber %10 == 0) || (userNumber %10 ==5))
+                {
+                    divBy5 = !divBy5;
+                }
+            }
+            return divBy5;
+        }
+        private static int cuntOfUpperCase(string i_userStr)
+        {
+            int countOfUpper = 0;
+            for (int i = 0; i < i_userStr.Length; i++)
+            {
+                if (char.IsUpper(i_userStr[i]))
+                {
+                    countOfUpper++;
+                }
+            }
+            return countOfUpper;
+        }
+        private static bool checkIfValidInput(string i_Str)
+        {
+            if (i_Str.Length != 8)
+            {
+                return false;
+            }
+            bool isNumber = false;
+            bool isChar = false;
+            bool isSign = false;
+            for (int i = 0; i < i_Str.Length; i++)
+            {
+                if (char.IsLetter(i_Str[i]))
+                {
+                    isChar = true;
+                }
+                else
+                    if (char.IsDigit(i_Str[i]))
+                {
+                    isNumber = true;
+                }
+                else
+                    isSign = true;
+            }
+            if (((isChar == true) && (isNumber == true))||(isSign == true))
+            {
+                return false;
+            }
+            return true;
+        }
+        private static string getStringFromUser()
+        {
+            Console.WriteLine("Please enter a string with 8 characters or digits:\n(note: not together)");
+            string userStr = Console.ReadLine();
+            while (checkIfValidInput(userStr) == false)
+            {
+                Console.WriteLine("Error: Invalid string (Can be only characters or numbers not together and the size must be 8)");
+                Console.WriteLine("Please try again:");
+                userStr = Console.ReadLine();
+            }
+            return userStr;
+        }
+        private static void stringAnalysis()
+        {
+            string userStr = getStringFromUser();
+            StringBuilder msg = new StringBuilder();
+            msg.AppendLine(
+               "=====================String Analysis=====================");
+            msg.AppendLine(
+                string.Format("Is the string a Palindrome: {0}", isPalindrome(userStr)));
+            /* Because the string contains only characters or numbers,
+            don't need to check the whole string but only the first character */
+            if (char.IsDigit(userStr[0]) == true)
+            {
+                msg.AppendLine(
+                    string.Format("Is it divisible by 5: {0}", isNumDivBy5(userStr)));
+            }
+            else
+            {
+                msg.AppendLine(
+                    string.Format("Number of Uppercases: {0}", cuntOfUpperCase(userStr)));
+            }
+
+            Console.WriteLine(msg);
+
+        }
+    }
+}
---
+++ B20_Ex01_5/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace B20_Ex01_05
+{
+    public class Program
+    {
+        public static void Main ()
+        {
+            numberStatistics();
+            Console.WriteLine("Please press 'ENTER' to exit...");
+            Console.ReadLine();
+        }
+
+        private static int getTheBigestDig(int i_userNumber)
+        {
+            int bigDig = 0;
+            while (i_userNumber > 0)
+            {
+                if (bigDig < i_userNumber % 10)
+                {
+                    bigDig = i_userNumber % 10;
+                }
+                i_userNumber = i_userNumber / 10;
+            }
+            return bigDig;
+        }
+
+        private static int getThelowestDig(int i_userNumber)
+        {
+            int smallestDig = 10;
+            while (i_userNumber > 0)
+            {
+                if (smallestDig > i_userNumber % 10)
+                {
+                    smallestDig = i_userNumber % 10;
+                }
+                i_userNumber = i_userNumber / 10;
+            }
+            return smallestDig;
+        }
+
+        private static int getTheCountOfDigDivBy3(int i_userNumber)
+        {
+            int countDivBy3 = 0;
+            while (i_userNumber > 0)
+            {
+                if (i_userNumber % 10 % 3 == 0)
+                {
+                    countDivBy3++;
+                }
+                i_userNumber = i_userNumber / 10;
+            }
+            return countDivBy3;
+        }
+        private static int getTheCountOfDigitUpperThenUnityDigit(int i_userNumber)
+        {
+            int countOfDigitUpperThenUnityDigit = 0;
+            int unityDigit = i_userNumber % 10;
+            i_userNumber = i_userNumber / 10;
+            while (i_userNumber > 0)
+            {
+                if (unityDigit < i_userNumber % 10)
+                {
+                    countOfDigitUpperThenUnityDigit++;
+                }
+                i_userNumber = i_userNumber / 10;
+            }
+            return countOfDigitUpperThenUnityDigit;
+        }
+        private static bool checkIfValidInputFromUser(string i_userInput)
+        {
+            bool checkIfItsANumber = false;
+            if (i_userInput.Length != 9)
+            {
+                return checkIfItsANumber;
+            }
+            int userNumber = 0;
+            checkIfItsANumber = int.TryParse(i_userInput, out userNumber);
+            if ((checkIfItsANumber == false) || (userNumber < 0))
+            {
+                return false;
+            }
+            return true;
+          }
+        private static string getInputFromUser()
+        {           
+            Console.WriteLine("Plase enter 9-digits positiv number:");
+            string userInput= Console.ReadLine();
+
+            while (checkIfValidInputFromUser(userInput) == false)
+            {
+                Console.WriteLine("Error!\nInvalid input (Can be only positive numbers and the size must be 9)\n----------------------------------\nPlease try again:");
+                userInput = Console.ReadLine();
+            }
+            return userInput;
+        }
+        private static void numberStatistics()
+        {
+            string userInput = getInputFromUser();
+            int userNumber = int.Parse(userInput);
+            string msg = string.Format(
+@"------------------Number Statistics--------------
+The number Is : {0}
+The bigest digit is : {1}
+The smallest digit is : {2}
+The number of digit divided by 3 is : {3}
+The number of digit bigger then unit digit is : {4} ",
+            userNumber,
+            getTheBigestDig(userNumber),
+            getThelowestDig(userNumber),
+            getTheCountOfDigDivBy3(userNumber),
+            getTheCountOfDigitUpperThenUnityDigit(userNumber));
+
+            Console.WriteLine(msg);
+        }
+
+    }
+}
+
+
